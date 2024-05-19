572ed37a7302f54cc5770f667aae5804518ae111
Justin Hartman <me@justhart.com>
2024-05-19 11:50:36 +0200

    docs: Add documentation to routes

--- app.js
+++ app.js
@@ -11,7 +11,13 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
 
-// Home route
+/**
+ * Handles the '/' route.
+ * This route is responsible for rendering the home page with new movies and TV shows.
+ * @param {Request} req - Express request object containing the request parameters and query string.
+ * @param {Response} res - Express response object used to send the rendered home page.
+ * @returns {void} - No return value.
+ */
 app.get('/', async (req, res) => {
     let newMovies = [];
     let newTVShows = [];
@@ -54,7 +60,13 @@ app.get('/', async (req, res) => {
     res.render('index', { newMovies, newTVShows, query: '', type: '', results: [] });
 });
 
-// Search route
+/**
+ * Handles the '/search' route.
+ * This route is responsible for searching for movies or TV shows based on the provided query.
+ * @param {Request} req - Express request object containing the request parameters and query string.
+ * @param {Response} res - Express response object used to send the rendered search results page.
+ * @returns {void} - No return value.
+ */
 app.get('/search', async (req, res) => {
     const query = req.query.q;
     const type = req.query.type || 'movie';
@@ -72,8 +84,14 @@ app.get('/search', async (req, res) => {
     res.render('index', { query, type, results, newMovies: [], newTVShows: [] });
 });
 
-// View route
-app.get('/view/:id', (req, res) => {
+/**
+ * Handles the '/view/:id' route.
+ * This route is responsible for rendering the view page for a specific video.
+ * @param {Request} req - Express request object containing the request parameters and query string.
+ * @param {Response} res - Express response object used to send the rendered view page.
+ * @returns {void} - No return value.
+ */
+app.get('/view/:id', async (req, res) => {
     const id = req.params.id;
     let type = req.query.type || 'movie';
     if (type === 'series') { type = 'tv' }
@@ -83,6 +101,11 @@ app.get('/view/:id', (req, res) => {
     res.render('view', { iframeSrc, title });
 });
 
+/**
+ * Starts the server and listens on the specified port.
+ * @param {Number} PORT - The port number on which the server will listen on.
+ * @returns {void} - No return value.
+ */
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
