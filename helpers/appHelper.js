62318845abae14e655df8f758dce33300a5339c5
Justin Hartman <me@justhart.com>
2024-05-25 18:53:22 +0200

    refactor: New fetchOmdbData method to simplify getting data

--- helpers/appHelper.js
+++ helpers/appHelper.js
@@ -1,19 +1,36 @@
 const axios = require('axios');
+const asyncHandler = require('express-async-handler');
 const config = require('../config/app');
 
 /**
- * Fetches the poster image URL for a movie or TV show using the OMDB API.
- * @param {string} imdbID - The IMDb ID of the movie or TV show.
- * @returns {Promise<string|null>} A Promise that resolves to the poster image URL or null if an error occurs.
+ * Constructs the OMDB API URL based on the query and type parameters.
+ * @param {string} query - The query string to search for movies or TV shows.
+ * @param {boolean} [search=true] - Search or standard query, default true.
+ * @param {string} type - The type of media to search for ('movie' or 'series'), default empty.
+ * @returns {string} The constructed OMDB API URL.
  */
-const getPoster = async (imdbID) => {
-    try {
-        const response = await axios.get(`http://www.omdbapi.com/?apikey=${config.OMDB_API_KEY}&i=${imdbID}`);
-        return response.data.Poster || '/images/no_image_available.png';
-    } catch (error) {
-        console.error('Error fetching poster:', error);
-        return null;
-    }
+function constructOmdbUrl(query, search = true, type = '') {
+  let url = `https://www.omdbapi.com/?apikey=${config.OMDB_API_KEY}&s=${query}&type=${type}`;
+  if (type === '' && search === false) {
+    url = `https://www.omdbapi.com/?apikey=${config.OMDB_API_KEY}&i=${query}`
+  } else if (type === '' && search === true) {
+    url = `https://www.omdbapi.com/?apikey=${config.OMDB_API_KEY}&s=${query}`
+  }
+  return url;
+}
+
+/**
+ * Fetches search results from OMDB API.
+ * @param {string} query - The query string to search for movies or TV shows.
+ * @param {boolean} [search=true] - Search or standard query, default true.
+ * @param {string} type - The type of media to search for ('movie' or 'series'), default empty.
+ * @returns {Promise<Array>} A Promise that resolves to an array of search results or an empty array if no results are found.
+ */
+const fetchOmdbData = async (query, search = true, type = '') => {
+  if(!query) return [];
+  const url = constructOmdbUrl(query, search, type);
+  const results = (await axios.get(url)).data || [];
+  return results || [];
 };
 
 /**
@@ -22,13 +39,13 @@ const getPoster = async (imdbID) => {
  * @returns {Promise<void>} A Promise that resolves when the poster images are fetched and updated.
  */
 const fetchAndUpdatePosters = async (show) => {
-    try {
-        await Promise.all(show.map(async (x) => {
-            x.poster = await getPoster(x.imdb_id);
-        }));
-    } catch (error) {
-        console.error('Error fetching and updating poster images:', error);
-    }
+  await Promise.all(show.map(async (x) => {
+    const data = await fetchOmdbData(x.imdb_id, false);
+    x.poster = data.Poster || '/images/no_image_available.png';
+  }));
 };
 
-module.exports = { getPoster, fetchAndUpdatePosters };
+module.exports = {
+  fetchAndUpdatePosters: asyncHandler(fetchAndUpdatePosters),
+  fetchOmdbData: asyncHandler(fetchOmdbData),
+};
