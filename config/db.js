64e2ad2201406db124470f82063219cd2b3cf2c5
Justin Hartman <me@justhart.com>
2024-05-30 21:34:59 +0200

    feat: Add Passport & MongoDB configuration

---
+++ config/db.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const appConfig = require('../config/app');
+
+/**
+ * Connects to the MongoDB database using Mongoose.
+ * @returns {Promise<void>} A Promise that resolves when the database is connected.
+ */
+const connectDB = async () => {
+  try {
+    /**
+     * Attempts to connect to the MongoDB database using the provided URI.
+     * @param {string} MONGO_DB_URI - The URI for the MongoDB database.
+     */
+    await mongoose.connect(appConfig.MONGO_DB_URI);
+    console.log('MongoDB Connected');
+  } catch (error) {
+    /**
+     * Handles any errors that occur during the database connection process.
+     * @param {Error} error - The error that occurred during the connection process.
+     */
+    console.error(error.message);
+    process.exit(1);
+  }
+};
+
+/**
+ * Exports the connectDB function for use in other parts of the application.
+ */
+module.exports = connectDB;
